b5ccb8113e56336534e2a17d915d0b7065997891
Mostafa <mostafa.livecode@gmail.com>
2021-01-13 21:20:34 +0200

    some updetes on all pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   'use strict';
+  $(document).on('click','.nav-item',function(){
+    $(this).addClass('active').siblings().removeClass();
+  })
 
   // Adjust slider height
   var winH = $(window).height(),
@@ -9,12 +12,24 @@ $(document).ready(function(){
 
 
   $('.carousel').carousel({
-    interval: 5000
+    interval: 500000
   })
+
+
+
   
 
 });
 
+let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', function () {
+    // spinnerWrapper.style.display = 'none';
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+});
+
 window.onload=function(){
   document.getElementById("my_audio").play();
+
 }
+
